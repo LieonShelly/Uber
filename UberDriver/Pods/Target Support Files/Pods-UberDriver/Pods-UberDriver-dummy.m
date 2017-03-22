b4cb1c9a79008daa6f1f9ec44a764e87d92170d9
lieon <lieoncx@gmail.com>
2017-03-22 23:39:13 +0800

    新建两个工程

---
+++ UberDriver/Pods/Target Support Files/Pods-UberDriver/Pods-UberDriver-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UberDriver : NSObject
+@end
+@implementation PodsDummy_Pods_UberDriver
+@end
---
+++ UberRider/Pods/Target Support Files/Pods-UberRider/Pods-UberRider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UberRider : NSObject
+@end
+@implementation PodsDummy_Pods_UberRider
+@end
